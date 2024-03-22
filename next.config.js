f5aa8d0bc5114944d3a6ae9acaa455b7b33656fc
DevOps Journey <151406815+devop-journey-uz@users.noreply.github.com>
2024-03-23 04:00:54 +0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
-/** @type {import('next').NextConfig} */ 
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
-    reactStrictMode: true,
-}
-module.exports = nextConfig
+	output: 'export',
+};
+
+module.exports = nextConfig;
